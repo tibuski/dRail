11bcf1cabd783a90b07e48149f851f63d0330fd2
tibuski <laurent.brichet@gmail.com>
2024-01-08 12:29:21 +0100

    add launch.json

--- dRail.py
+++ dRail.py
@@ -1,27 +1,41 @@
+
+import pytz
 import requests
-import json
+from datetime import datetime
 
-ROOT_URL= "https://api.irail.be/"
-
-def query_stations(response_format,response_lang):
-    
-    API_URL = ROOT_URL + "stations"
-    
-    query_result = requests.get(API_URL, params={'format': response_format, 'lang': response_lang})
-
-    return(query_result)
+ROOT_URL = "https://api.irail.be/"
+TZ = pytz.timezone('Europe/Brussels')
 
 def query_liveboard(what_to_query,response_format,response_lang,alerts_bool):
     
     API_URL = ROOT_URL + "liveboard"
     
-    query_result = requests.get(API_URL, params={'station': what_to_query, 'format': response_format, 'lang': response_lang, 'alerts': alerts_bool})
+    query_result = requests.get(
+        API_URL, 
+        params={
+            'station': what_to_query, 
+            'format': response_format, 
+            'lang': response_lang, 
+            'alerts': alerts_bool
+            }
+        )
 
     return(query_result)
 
-live_station = "Ath"
-liveboard = (query_liveboard(live_station, "json", "fr", "true")).json()
+live_station = "Schaerbeek"
 
-for departure in liveboard['departures']:
-    for idx in departure.len():
-        print(departure[idx])
+liveboard = (query_liveboard(live_station, "json", "en", "true")).json()
+
+departures = liveboard['departures']
+
+print(f"{'Time (Delay)' :<22} {'Station' :<25} {'Platform' :<}")
+print("===========================================================")
+
+for idx in departures['departure']:
+    
+    departureTime = datetime.fromtimestamp(int(idx['time']),tz=TZ)
+    departureDelay = "(+"+str(round(int((idx['delay']))/60))+")"
+    departurePlatform = idx['platform']
+
+    print(f"{departureTime.strftime('%H:%M')} {departureDelay:<15}  {idx['station']:<25} {departurePlatform:>6}")
+print("===========================================================")
