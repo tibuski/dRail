8e1625e32b16a16ce25de81697edbface602342a
Laurent Brichet <laurent.brichet@gmail.com>
2024-01-13 10:38:07 +0100

    update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git pull && \
+docker compose down && \
+docker system prune -af && \
+docker compose down && \
+docker compose build && \
+docker compose up -d && \
+docker compose logs -f
+
